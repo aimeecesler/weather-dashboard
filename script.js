f17c75972d9bfb28a29fa39d853c81ebb00e141e
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-12 13:33:44 -0400

    added event listener for submit button and functionality to add input value to the history array on submit, also added an id to the submit button to target it with

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
 // on submit, run a function that renders the city to the list along with other cities in local storage
 // add event listener for a click on a list item, run functions to render main card and 5 day forecast
 // add if statement that color codes the uv index
+
+// global variables
+var historyArr = [];
+
+$("#submit-btn").on("click",function(event){
+    event.preventDefault();
+    historyArr.push($("input").val());
+    console.log(historyArr);
+});
