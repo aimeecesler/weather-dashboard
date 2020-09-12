e36ddf4f0dfa77e21b5b2e6af16981963d275a7d
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-12 13:49:52 -0400

    added function to render main card with placeholder text, removed hard coded main card from html

--- script.js
+++ script.js
@@ -9,6 +9,33 @@
 // global variables
 var historyArr = [];
 
+function renderMainCard(){
+    var mainCard = $("<div>");
+    mainCard.addClass("card mt-3 mb-5");
+    var mainCardBody = $("<div>");
+    mainCardBody.addClass("card-body");
+    var mainCardTitle = $("<h2>");
+    mainCardTitle.addClass("card-title");
+    mainCardTitle.text("City Name");
+    var mainCardTemp = $("<h6>");
+    mainCardTemp.addClass("card-subtitle mt-4");
+    mainCardTemp.text("Temperature: ");
+    var mainCardHumid = $("<h6>");
+    mainCardHumid.addClass("card-subtitle mt-4");
+    mainCardHumid.text("Humidity: ");
+    var mainCardWind = $("<h6>");
+    mainCardWind.addClass("card-subtitle mt-4");
+    mainCardWind.text("Wind Speed: ");
+    var mainCardUV = $("<h6>");
+    mainCardUV.addClass("card-subtitle mt-4");
+    mainCardUV.text("UV Index: ");
+    mainCardBody.append(mainCardTitle, mainCardTemp, mainCardHumid, mainCardWind, mainCardUV);
+    mainCard.append(mainCardBody);
+    $("#main-card-div").append(mainCard);
+};
+
+renderMainCard();
+
 $("#submit-btn").on("click",function(event){
     event.preventDefault();
     historyArr.push($("input").val());
