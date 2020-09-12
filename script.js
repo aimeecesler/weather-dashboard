91475c3ecad499ed2371de36af347df8cf94822e
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-12 13:19:47 -0400

    added pseudo code for js file and changed color of submit button

---
+++ script.js
@@ -0,0 +1,7 @@
+// add event listener for clicks on the submit button
+// on submit, save the input to local storage (in an array?);
+// on submit, run a function that renders the main card and content
+// on submit, run a function that renders the 5 day forecast cards and content
+// on submit, run a function that renders the city to the list along with other cities in local storage
+// add event listener for a click on a list item, run functions to render main card and 5 day forecast
+// add if statement that color codes the uv index
